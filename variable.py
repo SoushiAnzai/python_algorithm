171cefdc67148b4623e755b8d96e3a28c6e566bd
soushi anzai <ahirusoushi@gmail.com>
2021-04-16 02:38:39 +0900

    variable

---
+++ variable.py
@@ -0,0 +1,39 @@
+# num = 1
+# name = 'Mike'
+# is_ok = True
+
+# print(num, type(num))
+# print(name, type(name))
+# print(is_ok, type(is_ok))
+
+# name = '1'
+# new_num = int(name)
+# print(new_num, type(new_num))
+
+# print('Hi','Mike', sep=",", end='.\n')
+# print('Hi','Mike', sep=",", end='.\n')
+
+# import math
+
+# result = math.sqrt(25)
+# print(result)
+# print(help(math))
+
+# print(r'C\name\name')
+
+# print("#################")
+# print("""\
+# line1
+# line2
+# line3\
+# """)
+# print("#################")
+
+# s = "My name is Mike. Hi Mike."
+# print(s)
+# print(s.replace('Mike', 'Madoka'))
+
+# print('My name is {name} {family}. 私は{family}{name}です。'.format(name='Madoka', family='Ishikura'))
+name = 'Madoka'
+family = 'Ishikura'
+print(f'My name is {name} {family}. 私は{family}{name}です。')
