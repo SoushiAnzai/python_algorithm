663859f24cb5c348ea9926b07a6389c72c10fd14
soushi anzai <ahirusoushi@gmail.com>
2021-04-17 22:17:38 +0900

    pythonパッケージ

---
+++ py_package.py
@@ -0,0 +1,11 @@
+# ranking = {
+#     'A': 100,
+#     'B': 85,
+#     'C': 95
+# }
+
+# print(sorted(ranking, key=ranking.get, reverse=True))
+
+from termcolor import colored
+
+print(colored('test', 'red'))
