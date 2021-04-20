c2fdaa5803bd4d34d81a6b450430371df874ebfb
soushi anzai <ahirusoushi@gmail.com>
2021-04-20 22:04:26 +0900

    バブルソート

--- algo/6_bubble/main.py
+++ algo/6_bubble/main.py
@@ -1,6 +1,15 @@
 from typing import List
 
 
+# def bubble_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     for i in range(len_numbers):
+#         for j in range(len_numbers - 1 - i):
+#             if numbers[j] > numbers[j + 1]:
+#                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+#     return numbers
+
 def bubble_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     for i in range(len_numbers):
