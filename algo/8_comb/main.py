1c8df0ec5dcdd37f21a2a8d3a6bd1052ba34ff26
soushi anzai <ahirusoushi@gmail.com>
2021-04-22 17:49:39 +0900

    comb

--- algo/8_comb/main.py
+++ algo/8_comb/main.py
@@ -1,16 +1,34 @@
 from typing import List
 
 
+# def comb_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     gap = len_numbers
+#     swapped = True
+
+#     while gap != 1 or swapped:
+#         gap = int(gap / 1.3)
+#         if gap < 1:
+#             gap = 1
+
+#         swapped = False
+
+#         for i in range(0, len_numbers - gap):
+#             if numbers[i] > numbers[i + gap]:
+#                 numbers[i], numbers[i + gap] = numbers[i + gap], numbers[i]
+#                 swapped = True
+#     return numbers
+
 def comb_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     gap = len_numbers
     swapped = True
-
+    
     while gap != 1 or swapped:
         gap = int(gap / 1.3)
         if gap < 1:
             gap = 1
-
+        
         swapped = False
 
         for i in range(0, len_numbers - gap):
