4e8d7fa4c2adff4fa8d7497538dcd6ea2a518828
soushi anzai <ahirusoushi@gmail.com>
2021-04-22 18:36:32 +0900

    selection_sort

--- algo/9_selection/main.py
+++ algo/9_selection/main.py
@@ -1,16 +1,24 @@
 from typing import List
 
 
+# def selection_sort(numbers: List[int]) -> List[int]:
+#     for i in range(len(numbers)):
+#         min_idx = i
+#         for j in range(i+1, len(numbers)):
+#             if numbers[min_idx] > numbers[j]:
+#                 min_idx = j
+
+#         numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
+#     return numbers
+
 def selection_sort(numbers: List[int]) -> List[int]:
     for i in range(len(numbers)):
         min_idx = i
         for j in range(i+1, len(numbers)):
             if numbers[min_idx] > numbers[j]:
                 min_idx = j
-
         numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
     return numbers
-
 
 if __name__ == '__main__':
     import random
