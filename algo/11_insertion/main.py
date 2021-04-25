5c2e93f3ffe900f5633b8aab818b2520d31ffaa2
soushi anzai <ahirusoushi@gmail.com>
2021-04-25 15:41:17 +0900

    insertion_sort

--- algo/11_insertion/main.py
+++ algo/11_insertion/main.py
@@ -1,18 +1,30 @@
 from typing import List
 
 
+# def insertion_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     for i in range(1, len_numbers):
+#         temp = numbers[i]
+#         j = i - 1
+
+#         while j >= 0 and numbers[j] > temp:
+#             numbers[j + 1] = numbers[j]
+#             j -= 1
+
+#         numbers[j + 1] = temp
+
+#     return numbers
+
 def insertion_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     for i in range(1, len_numbers):
         temp = numbers[i]
         j = i - 1
-
         while j >= 0 and numbers[j] > temp:
             numbers[j + 1] = numbers[j]
             j -= 1
-
         numbers[j + 1] = temp
-
+    
     return numbers
 
 
