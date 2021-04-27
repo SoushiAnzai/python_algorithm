5edbe3e0c3d7e0459f5c952ce6b1d8d6d8a0b388
soushi anzai <ahirusoushi@gmail.com>
2021-04-27 18:40:54 +0900

    shell_sort

--- algo/13_shell/main.py
+++ algo/13_shell/main.py
@@ -1,6 +1,21 @@
 from typing import List
 
 
+# def shell_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     gap = len_numbers // 2
+#     while gap > 0:
+#         for i in range(gap, len_numbers):
+#             temp = numbers[i]
+#             j = i
+#             while j >= gap and numbers[j-gap] > temp:
+#                 numbers[j] = numbers[j-gap]
+#                 j -= gap
+#             numbers[j] = temp
+#         gap //= 2
+
+#     return numbers
+
 def shell_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     gap = len_numbers // 2
@@ -12,8 +27,7 @@ def shell_sort(numbers: List[int]) -> List[int]:
                 numbers[j] = numbers[j-gap]
                 j -= gap
             numbers[j] = temp
-        gap //= 2
-
+        gap = gap // 2
     return numbers
 
 
